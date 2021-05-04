a6803abdadec4a924429c6646802973fc93ff866
Selvin Pérez <selvinperezxd@gmail.com>
2021-05-04 09:35:35 -0600

    No le entendi al progra :c

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{   int n;
+    for (int i=0; i<n;()++i){
+
+
+    cout << "Challenge #1" << endl;
+    int arreglo[] = {0, 1, 1, 1, 2, 2}; 
+    cout <<"el numero de repetidos es" <<n<<endl;
+
+     // Empiece con este arreglo ya existente, el docente modificará este arreglo para pruebas
+    return 0;
+}
+ /*
+     * Escriba un programa que remueva los elementos duplicados de cualquier arreglo, debe mostrar en pantalla el arreglo resultante
+     * Ejemplo 1: array = [1,2,2,3] array2 = [2,2] ---> Resultado: [1,2,3]
+     * Ejemplo 2: array = [0,0,0,1,2,2,3,3,3,3,3,3,4,4,4] ---> Resultado: [0,1,2,3,4]
+     * Consejo: Asuma que el arreglo que se probará siempre va a estar ordenado
+     */
